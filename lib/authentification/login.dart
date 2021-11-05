da7cfcafa855dc7df20d0c25540fe69986782aeb
Oskas <katembosyayanza@gmail.com>
2021-11-05 23:11:11 +0100

    CoorectionHttpRequestOnWeb

--- lib/authentification/login.dart
+++ lib/authentification/login.dart
@@ -59,7 +59,13 @@ class _LoginScreenState extends State<LoginScreen> {
         nomutilisateur +
         "&mot_de_passe=" +
         codeutilisateur;
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(Uri.parse(_url), headers: {
+      "Accept": "application/json",
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+    });
 
     setState(() {
       isLoading = false;
